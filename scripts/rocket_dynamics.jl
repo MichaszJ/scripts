946213d6e142c25778c0ac75b077e1416d88081c
MichaszJ <mjski@live.ca>
2021-12-07 01:06:01 -0500

    added nonimpulsive orbital maneuver function

--- scripts/rocket_dynamics.jl
+++ scripts/rocket_dynamics.jl
@@ -1,4 +1,4 @@
-include("solve_ode_ivp.jl")
+include("solve_ode.jl")
 
 function rocket_launch_trajectory(rocket_params, initial_conditions, t0, tf, thrust_func, ρ_func, D_func, g_func, m_func, h_pitchover; r_planet=6378.0e3, tolerance=50.0, beta=0.8, solver_method="rk_45")
     # defining the system of differential equations
@@ -53,4 +53,33 @@ function rocket_launch_trajectory(rocket_params, initial_conditions, t0, tf, thr
     s_out, t_out = rk_solver(differential_system, initial_conditions, t0, tf, solver_method=solver_method, tolerance=tolerance, beta=beta)
     
     return s_out, t_out
+end
+
+function diffeq_nonimpulsive_orbital_maneuver(initial_conditions, time_span, params; solver_args...)
+	function differential_system!(du, u, p, t)
+		# unpacking initial conditions and parameters
+		x, y, z, ẋ, ẏ, ż, m = u
+		μ, Isp, Tx_func, Ty_func, Tz_func = p
+	
+		# defining useful values
+		Tx = Tx_func(t, x, y, z, ẋ, ẏ, ż)
+		Ty = Ty_func(t, x, y, z, ẋ, ẏ, ż)
+		Tz = Tz_func(t, x, y, z, ẋ, ẏ, ż)
+		r = sqrt(x^2 + y^2 + z^2)
+		v = sqrt(ẋ^2 + ẏ^2 + ż^2)
+		
+		# defining system of DEs
+		du[1] = ẋ
+		du[2] = ẏ
+		du[3] = ż
+		du[4] = -μ*x / r^3 + (Tx / m) * (ẋ / v)
+		du[5] = -μ*y / r^3 + (Ty / m) * (ẏ / v)
+		du[6] = -μ*z / r^3 + (Tz / m) * (ż / v)
+		du[7] = -1e3 * sqrt(Tx^2 + Ty^2 + Tz^2) / (Isp * 9.80665)
+	end
+
+	problem = ODEProblem(differential_system!, initial_conditions, time_span, params)
+	solution = solve(problem; solver_args...)
+	
+	return solution
 end
