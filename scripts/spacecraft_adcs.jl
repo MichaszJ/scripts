34117806ef4192129106cd4639f919f69e35c78f
MichaszJ <mjski@live.ca>
2022-03-09 09:59:51 -0500

    added misc helper functions

--- scripts/spacecraft_adcs.jl
+++ scripts/spacecraft_adcs.jl
@@ -21,3 +21,17 @@ function par_3(θ)
         0 0 1
     ]
 end
+
+function cross_mat(v)
+    @assert length(v) == 3 "Vectors of length 3 required"
+    return [
+        0 -v[3] v[2]
+        v[3] 0 -v[1]
+        -v[2] v[1] 0
+    ]
+end
+
+function trace(matrix)
+	@assert size(matrix,1) == size(matrix,2) "Input must be a square matrix" 
+	return sum(matrix[i,i] for i in 1:size(matrix,1))
+end
